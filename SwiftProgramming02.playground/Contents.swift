a75d4952fdb0fb96458d9d325480688ed6b2ba64
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2017-01-26 10:27:24 -0500

    Chap -02 Types, Constants, Variables

---
+++ SwiftProgramming02.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//var numberOfLights = "Four"
+//var numberOfLights: Int = 4
+let numberOfStopLights: Int = 4
+//numberOfLights += 2
+var populations: Int
+populations = 5422
+var unemployedPeople = 1
+
+let townName: String = "Knowhere"
+let townDescription = "\(townName) has a population of \(populations) and \(numberOfStopLights) stoplights. The town has \(unemployedPeople) unemployed people."
+
+print(townDescription)
